a57dcdf8b238165d496f77da4d93d6cf26553269
LeotomasMC <marycolageo@gmail.com>
2018-03-11 14:55:13 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+#TheDerpyMemeBot
+#---------------
+
+## \/ DO NOT CHANGE THIS \/
+botType = "TheDerpyMemeBot"
+abr = "Alpha"
+ver = "3.8.01"
+verName = "Dis: Quotes System"
+
+nextUpdate = "Discord Things"
+
+version = botType + ' ' + abr + ' ' + ver
+## /\ DO NOT CHANGE THIS /\
+
+## General Settings Below
+hasNitro = False     # idk
+
+name = "TheDerpyMemeBot"
+
+clientID = "clientID"
+clientSecret = "clientSecret"
+
+username = "TheDerpyMemeBot"
+token = "token"
