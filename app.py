13cf058c5648ae2d11bf1de4398953855bfebf0f
LeotomasMC <marycolageo@gmail.com>
2018-07-17 14:01:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,22 @@ import os
 try:
     import discord
 except ImportError:
-    os.system('py -m pip install discord')
+    os.system('python -m pip install discord')
 
 try:
     import schedule
 except ImportError:
-    os.system('py -m pip install schedule')
+    os.system('python -m pip install schedule')
     
 try:
     import plyer
 except ImportError:
-    os.system('py -m pip install plyer')
+    os.system('python -m pip install plyer')
     
 try:
     import colorama
 except ImportError:
-    os.system('py -m pip install colorama')
+    os.system('python -m pip install colorama')
 
 
 import discordBot.py
