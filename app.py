a7d2032a03e45b900e6cc3654506d19fb99433ec
LeotomasMC <marycolageo@gmail.com>
2018-07-17 13:59:23 -0400

    Update app.py
    added pip installers for missing modules
    
    probably not the best way to do it

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
+import os
+try:
+    import discord
+except ImportError:
+    os.system('py -m pip install discord')
+
+try:
+    import schedule
+except ImportError:
+    os.system('py -m pip install schedule')
+    
+try:
+    import plyer
+except ImportError:
+    os.system('py -m pip install plyer')
+    
+try:
+    import colorama
+except ImportError:
+    os.system('py -m pip install colorama')
+
+
 import discordBot.py
